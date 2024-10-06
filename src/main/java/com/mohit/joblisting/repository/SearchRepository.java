b88887e85699d28a7282d284d17ab816f3f57686
Mohit Singh (mohisin3) <mohisin3@cisco.com>
2024-10-06 12:27:15 +0530

    first commit

---
+++ src/main/java/com/mohit/joblisting/repository/SearchRepository.java
@@ -0,0 +1,11 @@
+package com.mohit.joblisting.repository;
+
+import com.mohit.joblisting.model.Post;
+
+import java.util.List;
+
+public interface SearchRepository {
+
+    List<Post> findByText(String text);
+
+}
